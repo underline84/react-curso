42775b4862f7cfc0bdfb859da528bdcffa769c40
Evandro Gonçalves <84279332+underline84@users.noreply.github.com>
2022-06-17 16:25:28 -0300

    feature-5: implemented absolute imports and intregrated react-router

--- src/components/Promotion/Card/Card.js
+++ src/components/Promotion/Card/Card.js
@@ -3,7 +3,7 @@ import './Card.css';
 
 const PromotionCard = ({promotion}) => (
     <div className="promotion-card">
-        <img src={promotion.imageUrl} className="promotion-card__image" />
+        <img src={promotion.imageUrl} className="promotion-card__image" alt="" />
         <div className="promotion-card__info">
             <h1 className="promotion-card__title">{promotion.title}</h1>
             <span className="promotion-card__price">R$ {promotion.price}</span>
@@ -15,7 +15,7 @@ const PromotionCard = ({promotion}) => (
                     {promotion.comments.length}{' '} 
                     {promotion.comments.length > 1 ? 'Comentários' : 'Comentário'}</div>
 
-                <a href={promotion.url} target="_blank" className="promotion-card__link">IR PARA O SITE</a>
+                <a href={promotion.url} target="_blank" rel="noreferrer" className="promotion-card__link">IR PARA O SITE</a>
             </footer>
         </div>
     </div>
