b8e248dffa75fffade7e51102810f8be20eea87f
Evandro Gonçalves <84279332+underline84@users.noreply.github.com>
2022-06-20 15:10:17 -0300

    feature-6

---
+++ src/components/Promotion/List/List.js
@@ -0,0 +1,18 @@
+import React from "react";
+import PromotionCard from "../Card/Card";
+import './List.css';
+
+const PromotionList = ({loading, promotions}) => {
+    if(loading){
+        return <div>carregando...</div>;
+    }
+    return(
+        <div className="promotion-list">
+            {promotions.map((promotion) => (
+                <PromotionCard promotion={promotion}/>
+            ))}
+        </div>
+    )
+}
+
+export default PromotionList;
